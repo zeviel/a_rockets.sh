be69b3b18db7065ed706fdc4837d95c2221d4213
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-03-07 21:10:32 +0800

    Create a_rockets.sh

---
+++ src/a_rockets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+api="https://arockets.ru/data"
+
+function get_stores() {
+	curl --request GET \
+		--url "$api/vk_data.json" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--header "content-type: application/json"
+}
+
+function get_store_info() {
+	# 1 - store_id: (string): <store_id>
+	curl --request GET \
+		--url "$api/stores/$1.json" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--header "content-type: application/json"
+}
