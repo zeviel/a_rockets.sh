141931e688b91292fee4347687b7de2783bf03e5
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:05:53 +0800

    Update a_rockets.sh

--- src/a_rockets.sh
+++ src/a_rockets.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 api="https://arockets.ru/data"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function get_stores() {
 	curl --request GET \
 		--url "$api/vk_data.json" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json"
 }
 
@@ -13,6 +14,6 @@ function get_store_info() {
 	# 1 - store_id: (string): <store_id>
 	curl --request GET \
 		--url "$api/stores/$1.json" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json"
 }
